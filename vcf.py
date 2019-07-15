3d1efae4fef467d282676b29033814d849e5001a
Animesh Singh <animeshsingh.iitkgp@gmail.com>
2019-07-15 20:40:50 +0530

    Minor changes

--- vcf.py
+++ vcf.py
@@ -1,5 +1,4 @@
-from openpyxl import load_workbook
-from openpyxl import Workbook
+import csv
 import os.path
 
 def ask_user():
@@ -24,35 +23,35 @@ def vcfcreator(filename):                                   #main function
         return 0
     else:
         print("File found. :)\nProcessing...")
-        book  = load_workbook(filename)
-        print(book.sheetnames)                              #loading the file
-        sheet = book.active                                 #loading the sheet
-        file  = open("Contacts-llr.vcf","w+")               #creating the vcf file
+        with open(filename, 'r') as csvfile:             #loading the file               
+            spamreader = csv.reader(csvfile, delimiter=',')
+            for row in spamreader:
+                print(row)              
         
-        print(f"{sheet.cell(row=2, column=1).value} -- Is this the field containing the name?")
-        print(f"{(sheet.cell(row=2, column=2).value)} -- and this the field containing the room number?")
-        print(f"{int(sheet.cell(row=2, column=3).value)} -- and this the field containing the phone number?")
+        # print(f"{sheet.cell(row=2, column=1).value} -- Is this the field containing the name?")
+        # print(f"{(sheet.cell(row=2, column=2).value)} -- and this the field containing the room number?")
+        # print(f"{int(sheet.cell(row=2, column=3).value)} -- and this the field containing the phone number?")
         
-        if(ask_user() == True):
-            with open("sample.vcf", "w+") as file:
-                print(sheet.max_row)
-                for i in range(2, sheet.max_row+1):
-                    print(i)
-                    if(sheet.cell(row=i, column=1).value == None or sheet.cell(row=i, column=2).value == None or sheet.cell(row=i, column=3).value == None):
-                        print("One of the cell is empty.Check the file and try again  \nAborting...")
-                        return 0
-                    else:
-                        file.write("BEGIN:VCARD\n")
-                        file.write("VERSION:3.0\n")
-                        file.write(f"FN:{sheet.cell(row=i, column=1).value} {sheet.cell(row=i, column=2).value} LLR\n")
-                        file.write(f"N:{sheet.cell(row=i, column=1).value} {sheet.cell(row=i, column=2).value} LLR\n")
-                        file.write(f"TEL;TYPE=CELL:{int(sheet.cell(row=i, column=3).value)}\n")
-                        file.write(f"EMAIL:{(sheet.cell(row=i, column=4).value)}\n")
-                        file.write("END:VCARD\n")
+        # if(ask_user() == True):
+        #     with open("sample.vcf", "w+") as file:
+        #         print(sheet.max_row)
+        #         for i in range(2, sheet.max_row+1):
+        #             print(i)
+        #             if(sheet.cell(row=i, column=1).value == None or sheet.cell(row=i, column=2).value == None or sheet.cell(row=i, column=3).value == None):
+        #                 print("One of the cell is empty.Check the file and try again  \nAborting...")
+        #                 return 0
+        #             else:
+        #                 file.write("BEGIN:VCARD\n")
+        #                 file.write("VERSION:3.0\n")
+        #                 file.write(f"FN:{sheet.cell(row=i, column=1).value} {sheet.cell(row=i, column=2).value} LLR\n")
+        #                 file.write(f"N:{sheet.cell(row=i, column=1).value} {sheet.cell(row=i, column=2).value} LLR\n")
+        #                 file.write(f"TEL;TYPE=CELL:{int(sheet.cell(row=i, column=3).value)}\n")
+        #                 file.write(f"EMAIL:{(sheet.cell(row=i, column=4).value)}\n")
+        #                 file.write("END:VCARD\n")
 			
-            print("Write Successful.\nThanks for using !!")		
+        #     print("Write Successful.\nThanks for using !!")		
         
-        else:
-            print("Correct the file format and try again.")
+        # else:
+        #     print("Correct the file format and try again.")
 	
 vcfcreator("sample.csv")
