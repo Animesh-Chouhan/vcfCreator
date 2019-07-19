8117cfa3de7c44442dc02c14af0022b270be5f6a
Animesh Singh <animeshsingh.iitkgp@gmail.com>
2019-07-19 22:40:30 +0530

    sans formatter

--- vcf.py
+++ vcf.py
@@ -2,6 +2,9 @@ import csv
 import os.path
 
 def ask_user():
+    print(f"-- Is this the field containing the name?")
+    print(f"-- and this the field containing the room number?")
+    print(f"-- and this the field containing the phone number?")
     check = str(input("Reply in (Y/N): ")).lower().strip()
     try:
         if check[0] == 'y':
@@ -16,17 +19,67 @@ def ask_user():
         print(error)
         return ask_user()
 
+def encode(text):
+    encoded = text.replace('\\', '\\\\')
+    encoded = encoded.replace(',', '\\,')
+    return encoded
+#print(encode("this is one value,this \is another"))
+
+def name_formatter(name):
+    return name
 
 def vcfcreator(filename):                                   #main function
     if not os.path.isfile(filename):                        #checking if the file exists
         print("File doesn't exist. :(")
         return 0
     else:
+        data = []
         print("File found. :)\nProcessing...")
         with open(filename, 'r') as csvfile:             #loading the file               
-            spamreader = csv.reader(csvfile, delimiter=',')
-            for row in spamreader:
-                print(row)              
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                data.append(row)
+
+        print(data[0])
+        index = {}
+
+        attributes = {'FN': 'name',
+                    'NICKNAME': 'nick',
+                    'BDAY': 'birthday',
+                    'ANNIVERSARY': 'anniversary',
+                    'TEL': 'phone',
+                    'ADR': 'address',
+                    'EMAIL': 'email'
+                    }
+
+        with open("contacts.vcf", "w+") as file:
+            file.write("BEGIN:VCARD\n")
+            file.write("VERSION:4.0\n")
+
+            for row in data[1:]:
+                for attribute in attributes.keys():
+                    if attributes[attribute] in data[0]:
+                        index[attribute] = data[0].index(attributes[attribute])
+            print(index)
+
+            for i, row in enumerate(data[1:]):
+                for attribute in attributes.keys():
+                    if attributes[attribute] in data[0]:
+                        file.write("{}:{}\n".format(attribute, data[i+1][index[attribute]]))
+
+            file.write("END:VCARD\n")
+        
+        if(ask_user() == True):
+            print("Write Successful. Thanks for using :)")
+        else:
+            print("Raise an issue with the details and the vcf file created")
+            
+        
+
+
+        #char to encode "\\" / "\," / "\n"(Backslashes, commas, and newlines must be encoded)
+
+
         
         # print(f"{sheet.cell(row=2, column=1).value} -- Is this the field containing the name?")
         # print(f"{(sheet.cell(row=2, column=2).value)} -- and this the field containing the room number?")
