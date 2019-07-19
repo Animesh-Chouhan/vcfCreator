2ec65ea9c32bae0210acf13e89ff358e2829fdd2
Animesh Singh <animeshsingh.iitkgp@gmail.com>
2019-07-19 23:30:19 +0530

    clean up

--- vcf.py
+++ vcf.py
@@ -20,6 +20,7 @@ def ask_user():
         return ask_user()
 
 def encode(text):
+    #char to encode "\\" / "\," / "\n"(Backslashes, commas, and newlines must be encoded)
     encoded = text.replace('\\', '\\\\')
     encoded = encoded.replace(',', '\\,')
     return encoded
@@ -74,37 +75,6 @@ def vcfcreator(filename):                                   #main function
         else:
             print("Raise an issue with the details and the vcf file created")
             
-        
 
-
-        #char to encode "\\" / "\," / "\n"(Backslashes, commas, and newlines must be encoded)
-
-
-        
-        # print(f"{sheet.cell(row=2, column=1).value} -- Is this the field containing the name?")
-        # print(f"{(sheet.cell(row=2, column=2).value)} -- and this the field containing the room number?")
-        # print(f"{int(sheet.cell(row=2, column=3).value)} -- and this the field containing the phone number?")
-        
-        # if(ask_user() == True):
-        #     with open("sample.vcf", "w+") as file:
-        #         print(sheet.max_row)
-        #         for i in range(2, sheet.max_row+1):
-        #             print(i)
-        #             if(sheet.cell(row=i, column=1).value == None or sheet.cell(row=i, column=2).value == None or sheet.cell(row=i, column=3).value == None):
-        #                 print("One of the cell is empty.Check the file and try again  \nAborting...")
-        #                 return 0
-        #             else:
-        #                 file.write("BEGIN:VCARD\n")
-        #                 file.write("VERSION:3.0\n")
-        #                 file.write(f"FN:{sheet.cell(row=i, column=1).value} {sheet.cell(row=i, column=2).value} LLR\n")
-        #                 file.write(f"N:{sheet.cell(row=i, column=1).value} {sheet.cell(row=i, column=2).value} LLR\n")
-        #                 file.write(f"TEL;TYPE=CELL:{int(sheet.cell(row=i, column=3).value)}\n")
-        #                 file.write(f"EMAIL:{(sheet.cell(row=i, column=4).value)}\n")
-        #                 file.write("END:VCARD\n")
-			
-        #     print("Write Successful.\nThanks for using !!")		
-        
-        # else:
-        #     print("Correct the file format and try again.")
 	
 vcfcreator("sample.csv")
